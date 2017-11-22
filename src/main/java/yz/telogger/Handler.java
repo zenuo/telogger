121d41f7784b22c8a673783d826452e87b979db2
袁臻 <zenuor@gmail.com>
2017-11-22 15:30:19 +0800

    添加空闲事件

--- src/main/java/yz/telogger/Handler.java
+++ src/main/java/yz/telogger/Handler.java
@@ -2,35 +2,32 @@ package yz.telogger;
 
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
 
 import java.util.logging.Logger;
 
 @ChannelHandler.Sharable
-public final class Handler extends ChannelInboundHandlerAdapter {
+final class Handler extends SimpleChannelInboundHandler<String> {
 
     private Logger logger = Logger.getLogger(Handler.class.getName());
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         ClientManager.INSTANCE.add(ctx.channel());
-        logger.info("上线-" + ctx.channel().remoteAddress() + "\n目前客户端数量" + ClientManager.INSTANCE.count());
+        ctx.channel().writeAndFlush(Constant.HELLO);
+        logger.info("上线-" + ctx.channel().remoteAddress() + "\n当前客户端数量" + ClientManager.INSTANCE.count());
     }
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
         ClientManager.INSTANCE.remove(ctx.channel());
-        logger.info("离线-" + ctx.channel().remoteAddress() + "\n目前客户端数量" + ClientManager.INSTANCE.count());
-    }
-
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        //super.channelRead(ctx, msg);
+        logger.info("离线-" + ctx.channel().remoteAddress() + "\n当前客户端数量" + ClientManager.INSTANCE.count());
     }
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        //super.channelReadComplete(ctx);
     }
 
     @Override
@@ -38,5 +35,22 @@ public final class Handler extends ChannelInboundHandlerAdapter {
         logger.warning("客户端-" + ctx.channel().remoteAddress() + "发生异常：");
         cause.printStackTrace();
         ctx.close();
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        //若发生通道空闲事件
+        if (evt instanceof IdleStateEvent) {
+            IdleStateEvent idleStateEvent = (IdleStateEvent) evt;
+            if (idleStateEvent.state() == IdleState.WRITER_IDLE) {
+                ctx.channel().writeAndFlush(Constant.BYE);
+                //关闭通道
+                ctx.channel().close();
+            }
+        }
     }
 }
