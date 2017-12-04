80e46776839f4b11c54facd32e89d00010d85d95
袁臻 <zenuor@gmail.com>
2017-12-04 20:01:40 +0800

    修改测试

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -1,6 +1,18 @@
 import org.junit.Test;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
 import java.time.LocalDateTime;
 
 /**
@@ -32,4 +44,38 @@ public class Tester {
             Thread.sleep(1000L);
         }
     }
+
+    @Test
+    public void watch() throws IOException, InterruptedException {
+
+        WatchService watcher = FileSystems.getDefault().newWatchService();
+
+        final Path path = Paths.get("/home/user/project/omts/log");
+        path.register(
+                watcher,
+                StandardWatchEventKinds.ENTRY_CREATE,
+                StandardWatchEventKinds.ENTRY_DELETE,
+                StandardWatchEventKinds.ENTRY_MODIFY
+        );
+        while (true) {
+
+            WatchKey key = watcher.take();
+
+            for (WatchEvent<?> event : key.pollEvents()) {
+                System.out.println(event.kind().toString() + ":" + event.context());
+            }
+
+            boolean valid = key.reset();
+            if (!valid) {
+                break;
+            }
+        }
+
+    }
+
+    @Test
+    public void osname() {
+        System.out.println(System.getProperty("os.name"));
+    }
+
 }
