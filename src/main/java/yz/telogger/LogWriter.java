e7a3a6513ebe141ecf252f5f32675312911b237e
袁臻 <zenuor@gmail.com>
2017-12-09 23:47:38 +0800

    完善逻辑

--- src/main/java/yz/telogger/LogWriter.java
+++ src/main/java/yz/telogger/LogWriter.java
@@ -1,8 +1,6 @@
 package yz.telogger;
 
 import io.netty.channel.Channel;
-import io.netty.channel.group.ChannelMatcher;
-import io.netty.util.internal.ConcurrentSet;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -71,8 +69,7 @@ final class LogWriter {
         if (isNeedBoot()) {
             logger.info("启动" + filePath);
             boot();
-        }
-        if (isNeedShutDown()) {
+        } else if (isNeedShutDown()) {
             logger.info("关闭" + filePath);
             shutdown();
         }
@@ -81,10 +78,10 @@ final class LogWriter {
     /**
      * 是否需要启动
      *
-     * @return 当且仅当文件不为空且文件存在且未在工作中且存在订阅客户端时返回true，否则返回false
+     * @return 当且仅当未在工作中且存在订阅客户端时返回true，否则返回false
      */
     private boolean isNeedBoot() {
-        return path != null && Files.exists(path) && !isWorking && !setChannelMatcher.isEmpty();
+        return !isWorking && !setChannelMatcher.isEmpty();
     }
 
     /**
@@ -96,13 +93,21 @@ final class LogWriter {
         return isWorking && setChannelMatcher.isEmpty();
     }
 
+    /**
+     * 启动
+     */
     private void boot() {
+        //创建线程
         thread = new Thread(this::work);
         thread.setName("Thread-" + filePath);
         thread.setDaemon(true);
+        //开启线程
         thread.start();
     }
 
+    /**
+     * 关闭
+     */
     private void shutdown() {
         isWorking = false;
         if (process != null && process.isAlive()) {
@@ -115,69 +120,50 @@ final class LogWriter {
         }
     }
 
-    boolean contains(Object o) {
-        return setChannelMatcher.contains(o);
+    boolean contains(Channel o) {
+        return setChannelMatcher.matches(o);
     }
 
     /**
      * 监听文件直至异常发生
      */
     private void work() {
-        if (Files.exists(path)) {
-            //设置isWorking为true
-            isWorking = true;
-            //进程引用
-
-            try {
-                //创建进程
-                process = Runtime.getRuntime()
-                        .exec(String.format(Constant.TAIL_F_COMMAND, filePath));
-                logger.info("监听文件-" + filePath);
-                //监听进程的输出，阻塞至异常抛出或者被关闭输出
-                new BufferedReader(new InputStreamReader(process.getInputStream()))
-                        .lines()
-                        .forEach(line -> ClientManager.INSTANCE.writeLine(line, setChannelMatcher));
-            } catch (Exception e) {
-                logger.warning("监听文件-" + filePath + "-发生异常");
-                e.printStackTrace();
-            } finally {
-                //设置isWorking为false
-                isWorking = false;
-                //停止进程
-                if (process != null && process.isAlive()) {
-                    process.destroy();
-                }
+        //设置isWorking为true
+        isWorking = true;
+        //等待至文件存在
+        while (!Files.exists(path)) {
+            if (!isWorking) {
+                //若被关闭，结束
+                return;
             }
+            try {
+                Thread.sleep(2000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        //尝试读取文件内容
+        try {
+            //创建进程
+            process = Runtime.getRuntime()
+                    .exec(String.format(Constant.TAIL_F_COMMAND, filePath));
+            logger.info("监听文件-" + filePath);
+            //监听进程的输出，阻塞至异常抛出或者被关闭输出
+            new BufferedReader(new InputStreamReader(process.getInputStream()))
+                    .lines()
+                    .forEach(line -> ClientManager.INSTANCE.writeLine(line, setChannelMatcher));
+        } catch (Exception e) {
+            logger.warning("监听文件-" + filePath + "-发生异常");
+            e.printStackTrace();
+        } finally {
+            //设置isWorking为false
+            isWorking = false;
+            //检查状态
+            check();
         }
     }
 
     String getFilePath() {
         return filePath;
-    }
-}
-
-final class SetChannelMatcher implements ChannelMatcher {
-
-    private final ConcurrentSet<Channel> set = new ConcurrentSet<>();
-
-    void add(final Channel channel) {
-        set.add(channel);
-    }
-
-    void remove(final Channel channel) {
-        set.remove(channel);
-    }
-
-    boolean isEmpty() {
-        return set.isEmpty();
-    }
-
-    boolean contains(Object o) {
-        return set.contains(o);
-    }
-
-    @Override
-    public boolean matches(Channel channel) {
-        return set.contains(channel);
     }
 }
