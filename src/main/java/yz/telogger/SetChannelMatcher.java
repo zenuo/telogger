741a596467a9d8c7c79cf461aeb23e60e5ce14c2
袁臻 <zenuor@gmail.com>
2017-12-09 22:32:42 +0800

    分离集合channel匹配器

---
+++ src/main/java/yz/telogger/SetChannelMatcher.java
@@ -0,0 +1,34 @@
+package yz.telogger;
+
+import io.netty.channel.Channel;
+import io.netty.channel.group.ChannelMatcher;
+import io.netty.util.internal.ConcurrentSet;
+
+/**
+ * 集合channel匹配器
+ *
+ * @author 袁臻
+ * 2017/12/09 21:37
+ */
+public final class SetChannelMatcher implements ChannelMatcher {
+
+    private final ConcurrentSet<Channel> set = new ConcurrentSet<>();
+
+    void add(final Channel channel) {
+        set.add(channel);
+    }
+
+    void remove(final Channel channel) {
+        set.remove(channel);
+    }
+
+    boolean isEmpty() {
+        return set.isEmpty();
+    }
+
+    @Override
+    public boolean matches(Channel channel) {
+        return set.contains(channel);
+    }
+}
+
