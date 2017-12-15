96d2b95c9e3c209ef132329651dc68031e00b6b6
袁臻 <zenuor@gmail.com>
2017-12-15 17:56:06 +0800

    Add constant

--- src/main/java/yz/telogger/Constant.java
+++ src/main/java/yz/telogger/Constant.java
@@ -1,6 +1,7 @@
 package yz.telogger;
 
 import java.io.File;
+import java.util.regex.Pattern;
 
 /**
  * Constant class
@@ -11,7 +12,7 @@ import java.io.File;
 final class Constant {
 
     /**
-     * Configuration
+     * Configurations
      */
     static final boolean SSL = System.getProperty("ssl") != null;
 
@@ -23,30 +24,34 @@ final class Constant {
 
     static final String LOG_FILE_CONF_PATH = System.getProperty("logFileConfPath", "." + File.separatorChar + "logfile.conf");
 
-    static final String MESSAGE_HELLO = "-------Welcome to telogger-------\r\n";
+    static final String MESSAGE_HELLO = "-------Welcome to Telogger-------\r\n";
 
     static final String MESSAGE_BYE = "---------------Bye---------------\r\n";
 
     static final String NEW_LINE = "\r\n";
+
+    static final String EXTERNAL_COMMAND_DELIMITER = "#";
+
+    static final String COMMENT_SYMBOL = "//";
 
     static String TAIL_F_COMMAND = System.getProperty("os.name").toLowerCase().contains("windows") ?
             "\"C:\\Program Files\\Git\\usr\\bin\\tail.exe\" -n 0 --retry -f %s" :
             "tail -n 0 --retry -f %s";
 
     /**
-     * Internal command
+     * Internal commands
      */
-    static final String COMMAND_INTERNAL_SUBSCRIBE = "sub,,,subscribe a log file, if you already subscribed a log file," +
+    static final String COMMAND_INTERNAL_SUBSCRIBE = "sub###subscribe a log file, if you already subscribed a log file," +
             " please unsubscribe it first. e.g. sub {FileName}";
 
-    static final String COMMAND_INTERNAL_UNSUBSCRIBE = "unsub,,,unsubscribe the file you subscribed.";
+    static final String COMMAND_INTERNAL_UNSUBSCRIBE = "unsub###unsubscribe the file you subscribed.";
 
-    static final String COMMAND_INTERNAL_HELP = "help,,,get the help message.";
+    static final String COMMAND_INTERNAL_HELP = "help###get the help message.";
 
-    static final String COMMAND_INTERNAL_QUIT = "quit,,,exit the session.";
+    static final String COMMAND_INTERNAL_QUIT = "quit###exit the session.";
 
     /**
-     * Error message
+     * Error messages
      */
     static final String ERROR_COMMAND_NOT_FOUND = "Error-command not found: %s";
 
@@ -61,10 +66,16 @@ final class Constant {
             "it and retry";
 
     /**
-     * Success message
+     * Success messages
      */
     static final String SUCCESS_SUBSCRIBED = "Success-Subscribed log file: %s";
 
     static final String SUCCESS_UNSUBSCRIBED = "Success-Unsubscribed log file: %s";
+
+    /**
+     * Regex pattern
+     */
+    static final Pattern PATTERN_EXTERNAL_COMMAND = Pattern.compile("^(?<name>.+?)#(?<commandString>.+?)#" +
+            "(?<workingDirectory>.+?)#(?<help>.+?)$");
 
 }
