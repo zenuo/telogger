5bb52b02f48fa8a3bb6a8060a6a1f16d9849e5f9
袁臻 <zenuor@gmail.com>
2017-12-15 17:55:43 +0800

    Refactoring and translated

--- src/main/java/yz/telogger/Handler.java
+++ src/main/java/yz/telogger/Handler.java
@@ -8,33 +8,38 @@ import io.netty.handler.timeout.IdleStateEvent;
 import java.util.Objects;
 import java.util.logging.Logger;
 
+/**
+ * The channel inbound handler
+ *
+ * @author yziyz
+ * 2017/11/21 00:56
+ */
 final class Handler extends SimpleChannelInboundHandler<String> {
 
-    private final Logger logger = Logger.getLogger(Handler.class.getName());
+    /**
+     * Log
+     */
+    private final Logger log = Logger.getLogger(Handler.class.getName());
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        //添加客户端
+        //add client
         ClientManager.INSTANCE.add(ctx.channel());
-        //打印欢迎和帮助信息
-        ctx.channel().writeAndFlush(Constant.HELLO + Constant.NEW_LINE + CommandManager.INSTANCE.help());
-        logger.info("上线-" + ctx.channel().remoteAddress() + "\n当前客户端数量" + ClientManager.INSTANCE.count());
+        //send welcome and help message to client
+        ctx.channel().writeAndFlush(Constant.MESSAGE_HELLO + Constant.NEW_LINE + CommandManager.INSTANCE.help());
+        log.info("Online-" + ctx.channel().remoteAddress() + "\nOnline client count: " + ClientManager.INSTANCE.count());
     }
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-        //移除客户端
+        //remove client
         ClientManager.INSTANCE.remove(ctx.channel());
-        logger.info("离线-" + ctx.channel().remoteAddress() + "\n当前客户端数量" + ClientManager.INSTANCE.count());
-    }
-
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        log.info("Offline-" + ctx.channel().remoteAddress() + "\nOnline client count: " + ClientManager.INSTANCE.count());
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        logger.warning("客户端-" + ctx.channel().remoteAddress() + "发生异常");
+        log.warning("Error-" + ctx.channel().remoteAddress() + " exception occurred");
         cause.printStackTrace();
         ctx.close();
     }
@@ -43,28 +48,26 @@ final class Handler extends SimpleChannelInboundHandler<String> {
     protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
         final String trimMsg = msg.trim();
         if (trimMsg.length() != 0) {
-            logger.info("客户端-" + ctx.channel().remoteAddress() + "-执行命令-" + trimMsg);
-            //若客户端发送的字符串去除两端的空白后非空，则执行该字符串
-            if (Objects.equals(trimMsg, "quit")) {
+            if (Objects.equals(trimMsg.toLowerCase(), "quit")) {
                 //打印再见信息
-                ctx.channel().writeAndFlush(Constant.BYE);
+                ctx.channel().writeAndFlush(Constant.MESSAGE_BYE);
                 //关闭通道
                 ctx.channel().close();
             } else {
                 final String output = CommandManager.INSTANCE.exec(ctx.channel(), trimMsg);
-                ctx.writeAndFlush(output.concat(Constant.NEW_LINE));
+                ctx.writeAndFlush(output);
             }
         }
     }
 
     @Override
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
-        //若发生通道空闲事件
+        //Idle state event triggered
         if (evt instanceof IdleStateEvent) {
             final IdleStateEvent idleStateEvent = (IdleStateEvent) evt;
             if (idleStateEvent.state() == IdleState.WRITER_IDLE) {
-                ctx.channel().writeAndFlush(Constant.BYE);
-                //关闭通道
+                ctx.channel().writeAndFlush(Constant.MESSAGE_BYE);
+                //close channel
                 ctx.channel().close();
             }
         }
