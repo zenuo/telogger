484415ab078e69767dcdcc7f5242021f3eb99174
袁臻 <zenuor@gmail.com>
2017-12-15 16:14:43 +0800

    Refactoring and translated

--- src/main/java/yz/telogger/CommandManager.java
+++ src/main/java/yz/telogger/CommandManager.java
@@ -15,75 +15,117 @@ import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /**
- * 命令管理器
+ * The manager of command, singleton.
  *
- * @author 袁臻
+ * @author yziyz
  * 2017/12/05 18:01
  */
 public enum CommandManager {
 
     /**
-     * 单例
+     * Instance of command manager.
      */
     INSTANCE;
 
-    private final Logger logger = Logger.getLogger(CommandManager.class.getName());
+    /**
+     * Log of this instance.
+     */
+    private final Logger log = Logger.getLogger(CommandManager.class.getName());
 
-    private final ConcurrentHashMap<String, Command> map = new ConcurrentHashMap<>();
+    /**
+     * The mapping from command name to its instance
+     */
+    private final ConcurrentHashMap<String, Command> commandNameToInstanceMap = new ConcurrentHashMap<>();
 
+    /**
+     * The help message.
+     */
     private String help = null;
 
     /**
-     * 构造方法，实现读取文件到私有映射中
+     * Initialize, load command to commandNameToInstanceMap.
      */
-    CommandManager() {
-        //加载内部命令
-        //订阅文件
-        final Command subscribe = new Command(Constant.COMMAND_INTERNAL_SUBSCRIBE, true);
-        map.put(subscribe.getName(), subscribe);
-        //取消订阅文件
-        final Command unsubscribe = new Command(Constant.COMMAND_INTERNAL_UNSUBSCRIBE, true);
-        map.put(unsubscribe.getName(), unsubscribe);
-        //帮助
-        final Command help = new Command(Constant.COMMAND_INTERNAL_HELP, true);
-        map.put(help.getName(), help);
-        //退出
-        final Command quit = new Command(Constant.COMMAND_INTERNAL_QUIT, true);
-        map.put(quit.getName(), quit);
+    void init() {
+        //load commands
+        loadInternalCommand();
+        loadExternalCommand();
+        //log command names list
+        final List<String> commandNameList = commandNameToInstanceMap.values()
+                .stream()
+                .map(Command::getName)
+                .collect(Collectors.toList());
+        log.info("Commands: " + commandNameList);
+    }
 
-        //加载外部命令
-        //若文件存在，则加载外部命令
+    /**
+     * Load internal commands
+     */
+    private void loadInternalCommand() {
+        //load internal command
+        //command 'sub'
+        final Command subscribe = new Command(Constant.COMMAND_INTERNAL_SUBSCRIBE, true);
+        commandNameToInstanceMap.put(subscribe.getName(), subscribe);
+        //command 'unsub'
+        final Command unsubscribe = new Command(Constant.COMMAND_INTERNAL_UNSUBSCRIBE, true);
+        commandNameToInstanceMap.put(unsubscribe.getName(), unsubscribe);
+        //command 'help'
+        final Command help = new Command(Constant.COMMAND_INTERNAL_HELP, true);
+        commandNameToInstanceMap.put(help.getName(), help);
+        //command 'quit'
+        final Command quit = new Command(Constant.COMMAND_INTERNAL_QUIT, true);
+        commandNameToInstanceMap.put(quit.getName(), quit);
+    }
+
+    /**
+     * Load external commands
+     */
+    private void loadExternalCommand() {
+        //load external command
+        //if command configuration file exists, load it
         final Path path = Paths.get(Constant.COMMAND_CONF_PATH);
         if (Files.exists(path)) {
             try {
                 Files.lines(path)
                         .map(String::trim)
-                        .filter(line -> !line.startsWith("#") && line.length() != 0)
+                        .filter(this::isValidExternalCommand)
                         .map(line -> new Command(line, false))
-                        .forEach(command -> map.put(command.getName(), command));
+                        .forEach(command -> commandNameToInstanceMap.put(command.getName(), command));
             } catch (IOException e) {
-                logger.warning("读取命令时发生异常");
+                log.warning("Exception occurred while loading external command:");
                 e.printStackTrace();
             }
+        } else {
+            log.info("External command not configured.");
         }
     }
 
     /**
-     * 获取帮助字符串
+     * Get help message
      *
-     * @return 根据映射构建帮助字符串
+     * @return help message string
      */
     String help() {
+        //lazy initialize
         if (help == null) {
-            final StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("可用命令:").append(Constant.NEW_LINE);
-            map.forEach((key, command) ->
-                    stringBuilder.append(
-                            String.format("%-20s %s\r\n", command.getName(), command.getHelp())
-                    )
-            );
+            //new StringBuilder
+            final StringBuilder stringBuilder = new StringBuilder("Commands:")
+                    .append(Constant.NEW_LINE);
+            //append commands info
+            stringBuilder.append(commandNameToInstanceMap.values()
+                    .stream()
+                    .collect(Collector.of(
+                            StringBuilder::new,
+                            (StringBuilder sb, Command c) -> sb
+                                    .append(String.format("%-20s %s\r\n", c.getName(), c.getHelp())),
+                            StringBuilder::append,
+                            Collector.Characteristics.IDENTITY_FINISH
+                    )));
+            //assign stringBuilder to help
             help = stringBuilder
                     .append(Constant.NEW_LINE)
                     .append(LogWriterManager.INSTANCE.filePaths())
@@ -93,44 +135,45 @@ public enum CommandManager {
     }
 
     /**
-     * 执行传入的命令
+     * Execute the command that client requested
      *
-     * @param commandLineString 命令行字符串
-     * @return 若存在对应的命令，则执行它并返回其输出字符串；否则返回帮助字符串
+     * @param commandLineString string client requested
+     * @return if command found, execute it, return its output, otherwise return error message
      */
     String exec(final Channel channel, final String commandLineString) {
-        //使用空格切分命令行字符串
+        //split commandLineString by space
         final List<String> segments = new ArrayList<>(Arrays.asList(commandLineString.split(" ")));
-        //命令字符串
+        //get command name
         final String commandString = segments.remove(0);
-        //获取命令实例
-        final Command command = map.get(commandString);
-        //判断是否存在
+        //log
+        log.info("Command-" + channel.remoteAddress() + "-" + commandLineString);
+        //get command instance
+        final Command command = commandNameToInstanceMap.get(commandString);
+        //judge whether it found or not
         if (command != null) {
-            //命令存在
-            logger.info("执行命令-" + commandString + "-" + segments.toString());
+            //found
             if (command.isInternal()) {
-                //内部命令
-                return execInternal(channel, commandString, segments);
+                //internal command
+                return execInternalCommand(channel, commandString, segments);
             } else {
-                //外部命令
-                return execExternal(command, segments);
+                //external command
+                return execExternalCommand(command, segments);
             }
         } else {
-            //命令不存在
-            return String.format(Constant.ERROR_COMMAND_NOT_EXISTS, commandString, help());
+            //not found
+            return String.format(Constant.ERROR_COMMAND_NOT_FOUND, commandString);
         }
     }
 
     /**
-     * 执行内部命令
+     * Execute internal command
      *
-     * @param channel       请求执行命令的channel
-     * @param commandString 命令字符串
-     * @param arguments     参数字符串列表
-     * @return 返回给客户端的信息
+     * @param channel       the channel that
+     * @param commandString command string
+     * @param arguments     argument string list
+     * @return output
      */
-    private String execInternal(final Channel channel, final String commandString, final List<String> arguments) {
+    private String execInternalCommand(final Channel channel, final String commandString, final List<String> arguments) {
         switch (commandString) {
             case "sub":
                 return LogWriterManager.INSTANCE.subscribe(channel, arguments);
@@ -139,41 +182,63 @@ public enum CommandManager {
             case "help":
                 return help();
             default:
-                return String.format(Constant.ERROR_COMMAND_NOT_EXISTS, commandString, help());
+                return String.format(Constant.ERROR_COMMAND_NOT_FOUND, commandString);
         }
     }
 
     /**
-     * 执行外部命令
+     * Execute external command
      *
-     * @param command   命令字符串
-     * @param arguments 参数字符串列表
-     * @return 返回给客户端的信息
+     * @param command   command instance
+     * @param arguments argument string list
+     * @return output
      */
-    private String execExternal(final Command command, final List<String> arguments) {
+    private String execExternalCommand(final Command command, final List<String> arguments) {
+        //new StringBuilder
         final StringBuilder stringBuilder = new StringBuilder();
+        //the process that executes command
         Process process = null;
         try {
-            //新建进程执行命令
+            //executes the specified string command in a separate process
             process = Runtime.getRuntime().exec(
                     command.getCommand(),
                     null,
                     new File(command.getWorkingDirectory())
             );
-            //读取输出，阻塞
+            //get stdout lines, blocking until process returned
             new BufferedReader(new InputStreamReader(process.getInputStream()))
                     .lines()
                     .forEach(line -> stringBuilder.append(line).append(Constant.NEW_LINE));
-            //等待进程结束
-            process.waitFor(1000L, TimeUnit.MILLISECONDS);
+            //blocking until process terminated
+            process.waitFor(20000L, TimeUnit.MILLISECONDS);
         } catch (Exception e) {
-            logger.warning("执行命令-异常-" + command.toString() + "-参数列表-" + arguments.toString());
+            //exception occurred
+            log.warning("Error-Command-" + command.toString() + "-arguments-" + arguments.toString());
             e.printStackTrace();
         } finally {
+            //clear process
             if (process != null && process.isAlive()) {
-                process.destroy();
+                process.destroyForcibly();
             }
         }
+        //return output
         return stringBuilder.toString();
+    }
+
+    /**
+     * Judge whether external command valid or not
+     *
+     * @param line a line of external command configuration
+     * @return true if valid, false otherwise
+     */
+    private boolean isValidExternalCommand(final String line) {
+        if (!line.isEmpty() && !line.startsWith(Constant.COMMENT_SYMBOL)) {
+            final Matcher matcher = Constant.PATTERN_EXTERNAL_COMMAND.matcher(line);
+            final boolean lineMatches = matcher.find();
+            final boolean workingDirectoryExists = Paths.get(matcher.group("workingDirectory")).toFile().exists();
+            return lineMatches && workingDirectoryExists;
+        } else {
+            return false;
+        }
     }
 }
