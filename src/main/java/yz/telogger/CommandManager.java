2de0ad5119d8a99d825ca874119e21b2b70d181c
袁臻 <zenuor@gmail.com>
2017-12-19 17:30:45 +0800

    Refactored internal and external commands

--- src/main/java/yz/telogger/CommandManager.java
+++ src/main/java/yz/telogger/CommandManager.java
@@ -13,7 +13,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.stream.Collector;
@@ -40,7 +42,9 @@ public enum CommandManager {
     /**
      * The mapping from command name to its instance
      */
-    private final ConcurrentHashMap<String, Command> commandNameToInstanceMap = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, Command> internalCommandNameToInstanceMap = new ConcurrentHashMap<>();
+
+    private final ConcurrentHashMap<String, Command> externalCommandNameToInstanceMap = new ConcurrentHashMap<>();
 
     /**
      * The help message.
@@ -52,13 +56,20 @@ public enum CommandManager {
      */
     void init() {
         //load commands
-        loadInternalCommand();
+        if (internalCommandNameToInstanceMap.isEmpty()) {
+            loadInternalCommand();
+        }
         loadExternalCommand();
         //log command names list
-        final List<String> commandNameList = commandNameToInstanceMap.values()
+        final List<String> commandNameList = internalCommandNameToInstanceMap.values()
                 .stream()
                 .map(Command::getName)
                 .collect(Collectors.toList());
+        commandNameList.addAll(externalCommandNameToInstanceMap.values()
+                .stream()
+                .map(Command::getName)
+                .collect(Collectors.toList())
+        );
         log.info("Commands: " + commandNameList);
     }
 
@@ -69,32 +80,42 @@ public enum CommandManager {
         //load internal command
         //command 'sub'
         final Command subscribe = new Command(Constant.COMMAND_INTERNAL_SUBSCRIBE, true);
-        commandNameToInstanceMap.put(subscribe.getName(), subscribe);
         //command 'unsub'
         final Command unsubscribe = new Command(Constant.COMMAND_INTERNAL_UNSUBSCRIBE, true);
-        commandNameToInstanceMap.put(unsubscribe.getName(), unsubscribe);
         //command 'help'
         final Command help = new Command(Constant.COMMAND_INTERNAL_HELP, true);
-        commandNameToInstanceMap.put(help.getName(), help);
         //command 'quit'
         final Command quit = new Command(Constant.COMMAND_INTERNAL_QUIT, true);
-        commandNameToInstanceMap.put(quit.getName(), quit);
+        //command 'reload'
+        final Command reload = new Command(Constant.COMMAND_INTERNAL_RELOAD, true);
+        internalCommandNameToInstanceMap.put(subscribe.getName(), subscribe);
+        internalCommandNameToInstanceMap.put(unsubscribe.getName(), unsubscribe);
+        internalCommandNameToInstanceMap.put(help.getName(), help);
+        internalCommandNameToInstanceMap.put(quit.getName(), quit);
+        internalCommandNameToInstanceMap.put(reload.getName(), reload);
     }
 
     /**
      * Load external commands
      */
-    private void loadExternalCommand() {
+    void loadExternalCommand() {
         //load external command
         //if command configuration file exists, load it
         final Path path = Paths.get(Constant.COMMAND_CONF_PATH);
         if (Files.exists(path)) {
             try {
-                Files.lines(path)
+                final ConcurrentMap<String, Command> collect = Files.lines(path)
                         .map(String::trim)
                         .filter(this::isValidExternalCommand)
                         .map(line -> new Command(line, false))
-                        .forEach(command -> commandNameToInstanceMap.put(command.getName(), command));
+                        .collect(Collectors.toConcurrentMap(Command::getName, Function.identity()));
+                //if collect is not empty
+                if (!collect.isEmpty()) {
+                    //clear old content
+                    externalCommandNameToInstanceMap.clear();
+                    //put all new content
+                    externalCommandNameToInstanceMap.putAll(collect);
+                }
             } catch (IOException e) {
                 log.warning("Exception occurred while loading external command:");
                 e.printStackTrace();
@@ -107,21 +128,31 @@ public enum CommandManager {
     /**
      * Get help message
      *
+     * @param reload is reloading help message
      * @return help message string
      */
-    String help() {
+    String help(boolean reload) {
         //lazy initialize
-        if (help == null) {
+        if (help == null || reload) {
             //new StringBuilder
             final StringBuilder stringBuilder = new StringBuilder("Commands:")
                     .append(Constant.NEW_LINE);
             //append commands info
-            stringBuilder.append(commandNameToInstanceMap.values()
+            stringBuilder.append(internalCommandNameToInstanceMap.values()
                     .stream()
                     .collect(Collector.of(
                             StringBuilder::new,
                             (StringBuilder sb, Command c) -> sb
-                                    .append(String.format("%-20s %s\r\n", c.getName(), c.getHelp())),
+                                    .append(String.format("%-30s %s\r\n", c.getName(), c.getHelp())),
+                            StringBuilder::append,
+                            Collector.Characteristics.IDENTITY_FINISH
+                    )));
+            stringBuilder.append(externalCommandNameToInstanceMap.values()
+                    .stream()
+                    .collect(Collector.of(
+                            StringBuilder::new,
+                            (StringBuilder sb, Command c) -> sb
+                                    .append(String.format("%-30s %s\r\n", c.getName(), c.getHelp())),
                             StringBuilder::append,
                             Collector.Characteristics.IDENTITY_FINISH
                     )));
@@ -135,8 +166,22 @@ public enum CommandManager {
     }
 
     /**
+     * Re-opening log files
+     *
+     * @param channel the channel instance of the client requested
+     * @return message that send to the client
+     */
+    String reload(final Channel channel) {
+        log.info("Reload-" + channel.remoteAddress());
+        LogWriterManager.INSTANCE.init();
+        CommandManager.INSTANCE.init();
+        return help(true);
+    }
+
+    /**
      * Execute the command that client requested
      *
+     * @param channel           the channel instance of the client requested
      * @param commandLineString string client requested
      * @return if command found, execute it, return its output, otherwise return error message
      */
@@ -148,7 +193,10 @@ public enum CommandManager {
         //log
         log.info("Command-" + channel.remoteAddress() + "-" + commandLineString);
         //get command instance
-        final Command command = commandNameToInstanceMap.get(commandString);
+        Command command = internalCommandNameToInstanceMap.get(commandString);
+        if (command == null) {
+            command = externalCommandNameToInstanceMap.get(commandString);
+        }
         //judge whether it found or not
         if (command != null) {
             //found
@@ -168,7 +216,7 @@ public enum CommandManager {
     /**
      * Execute internal command
      *
-     * @param channel       the channel that
+     * @param channel       the channel instance of the client requested
      * @param commandString command string
      * @param arguments     argument string list
      * @return output
@@ -178,9 +226,11 @@ public enum CommandManager {
             case "sub":
                 return LogWriterManager.INSTANCE.subscribe(channel, arguments);
             case "unsub":
-                return LogWriterManager.INSTANCE.unsunscribe(channel);
+                return LogWriterManager.INSTANCE.unsubscribe(channel);
             case "help":
-                return help();
+                return help(false);
+            case "reload":
+                return reload(channel);
             default:
                 return String.format(Constant.ERROR_COMMAND_NOT_FOUND, commandString);
         }
